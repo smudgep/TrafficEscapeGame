ccae62f9459446f39d5264e82ab939b9fcdf092d
Roisin Pinches <roisinpinches@gmail.com>
2025-12-01 14:31:13 +0000

    Add project files.

---
+++ TrafficEscapeGame/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TrafficEscapeGame
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
