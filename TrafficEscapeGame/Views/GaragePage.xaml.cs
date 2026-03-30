98bbe5268a490b83f9fe374a91790eeed9e84de6
Roisin Pinches <roisinpinches@gmail.com>
2026-03-30 11:14:43 +0100

    Add simplified GaragePage with blue and red car selection

--- TrafficEscapeGame/Views/GaragePage.xaml.cs
+++ TrafficEscapeGame/Views/GaragePage.xaml.cs
@@ -1,14 +1,27 @@
 namespace TrafficEscapeGame.Views;
-//incomplete garage page
+
 public partial class GaragePage : ContentPage
 {
-	public GaragePage()
-	{
-		InitializeComponent();
-	}
-    //navigate back to main menu
+    public GaragePage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnSelectBlue(object sender, EventArgs e)
+    {
+        CarPreview.Source = "bluecar.png";
+        StatusLabel.Text = "Currently Selected: Blue";
+    }
+
+    private void OnSelectRed(object sender, EventArgs e)
+    {
+        CarPreview.Source = "redcar.png";
+        StatusLabel.Text = "Currently Selected: Red";
+    }
+
+    // Fixes the "not awaited" error by using await
     private async void OnMainMenuButton_Clicked(object sender, EventArgs e)
     {
-		await Navigation.PopToRootAsync();
+        await Navigation.PopAsync();
     }
 }
